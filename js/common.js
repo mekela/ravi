1d624c5f903aa61e42f24ea11c1a3f1f8ffad9a6
alex <sania_free@i.ua>
2016-06-15 16:18:19 +0300

    add last fix

--- js/common.js
+++ js/common.js
@@ -14,3 +14,16 @@ $(document).ready(function() {
 	});
 
 });
+
+$(document).ready(function() {
+	if($(".photo_block_about").length) {
+		var width = $(window).width();
+		if (width < 760) {
+			var div=$(".photo_block_about");
+			$(".photo_block_about").remove();
+			$(".photos_col").children().eq(0).after(div);
+			//console.log($(".photos_col").children().eq(0));
+			
+		}
+	}
+});
